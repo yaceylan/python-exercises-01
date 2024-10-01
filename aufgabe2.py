372e45da62d803ebb88edea7caa99e5e37fde11f
Yasemin <yacey@hotmail.de>
2024-10-01 16:19:48 +0200

    aufgabe2.py erstellt

---
+++ aufgabe2.py
@@ -0,0 +1,12 @@
+#Definieren der Variablen a und b
+a = 10
+b = 3
+
+
+#Berechnung und Ausgabe
+print("Summe:", a + b)
+print("Differenz:", a - b)
+print("Produkt:", a * b)
+print("Ganzzahliger Quotient:", a // b)
+print("Rest der Division:", a % b)
+print("a hoch b:", a ** b)
