18d88db8e82ef23a72229c6353e0d81b40de5c56
Yasemin <yacey@hotmail.de>
2024-10-01 14:58:11 +0200

    modified aufgabe1.py

---
+++ aufgabe1.py
@@ -0,0 +1,12 @@
+#Definieren von Variablen
+first_name = "yasemin"
+last_name = "ceylan"
+birth_year = 1989
+has_license = True
+
+#Variablenwerte ausgeben
+print (first_name)
+print (last_name)
+print (birth_year)
+print (has_license)
+
